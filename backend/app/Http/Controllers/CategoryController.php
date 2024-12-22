21bea2d7176e76e147c4beaeff6c7bff8c598b3b
Lorenzo Lucchesi <lorenzolucchesi3@gmail.com>
2024-12-22 12:37:23 +0100

    Complete CategoryController

--- backend/app/Http/Controllers/CategoryController.php
+++ backend/app/Http/Controllers/CategoryController.php
@@ -3,17 +3,22 @@
 namespace App\Http\Controllers;
 
 use App\Models\Category;
+use Illuminate\Http\JsonResponse;
 
 class CategoryController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return void
-     */
-    public function getCategories()
-    {
-        $categories = Category::all();
-        return response()->json($categories);
-    }
+	/**
+	 * Display a listing of the resource.
+	 *
+	 * @return JsonResponse
+	 */
+	public function getCategories(): JsonResponse
+	{
+		try {
+			$categories = Category::all();
+			return response()->json($categories);
+		} catch (\Throwable $e) {
+			return response()->json(['message' => 'Failed to get categories.'], 500);
+		}
+	}
 }
--- backend/app/Http/Controllers/WarehouseController.php
+++ backend/app/Http/Controllers/WarehouseController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers;
 
 use App\Models\Warehouse;
 use Illuminate\Http\JsonResponse;
-use Illuminate\Http\Request;
 
 class WarehouseController extends Controller
 {
