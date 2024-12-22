f59d51cfae0e608c5e41c3c8e0135e471b43af4d
Lorenzo Lucchesi <lorenzolucchesi3@gmail.com>
2024-12-22 12:36:11 +0100

    Complete WarehouseController

--- backend/app/Http/Controllers/WarehouseController.php
+++ backend/app/Http/Controllers/WarehouseController.php
@@ -3,19 +3,23 @@
 namespace App\Http\Controllers;
 
 use App\Models\Warehouse;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
 class WarehouseController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @param Request $request
-     * @return void
-     */
-    public function getWarehouses(Request $request)
-    {
-        $warehouses = Warehouse::all();
-        return response()->json($warehouses);
-    }
+	/**
+	 * Display a listing of the resource.
+	 *
+	 * @return JsonResponse
+	 */
+	public function getWarehouses(): JsonResponse
+	{
+		try {
+			$warehouses = Warehouse::all();
+			return response()->json($warehouses);
+		} catch (\Throwable $e) {
+			return response()->json(['message' => 'Failed to get warehouses.'], 500);
+		}
+	}
 }
